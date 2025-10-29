d65e2872f81281e8ab131d4abb54d7823bf9d8bb
Kurt Pascua <kurtreonal26@gmail.com>
2025-10-30 07:48:18 +0800

    add gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), 'babel-plugin-react-compiler',] // must run first!,
+  base: "/portfolio-react-tw/",
+
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          "babel-plugin-react-compiler"
+        ],
+      },
+    }),
+    tailwindcss(),
+  ],
 })
