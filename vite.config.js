bb35d4e1dc20634a663a413318674dd01d27b230
Kurt Pascua <kurtreonal26@gmail.com>
2025-10-30 09:22:17 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const isGitHubPages = process.env.VITE_APP_HOSTING === 'ghpages';
+const isGitHubPages = process.env.VITE_APP_HOSTING === 'ghpages'
 
 export default defineConfig({
-  base: isGitHubPages
-    ? "/portfolio-react-tw/"
-    : "/",
-
+  base: isGitHubPages ? '/portfolio-react-tw/' : '/',
   plugins: [
     react({
-      babel: {
-        plugins: [
-          "babel-plugin-react-compiler"
-        ],
-      },
+      babel: { plugins: ['babel-plugin-react-compiler'] },
     }),
     tailwindcss(),
   ],
