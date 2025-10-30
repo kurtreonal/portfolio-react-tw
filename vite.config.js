1ae57dd2d3c8fb668ee864bfe66ec5c582511e64
Kurt Pascua <kurtreonal26@gmail.com>
2025-10-30 08:07:14 +0800

    Commit changes deployed on ithub + vercel

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const isGitHubPages = process.env.VITE_APP_HOSTING === 'ghpages';
+
 export default defineConfig({
-  base: "/portfolio-react-tw/",
+
+
+  base: isGitHubPages
+    ? "/portfolio-react-tw/"
+    : "/",
   plugins: [
     react({
       babel: {
